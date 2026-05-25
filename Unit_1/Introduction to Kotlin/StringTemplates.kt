a493c2fd7e100b104a08301f40411986c3792477
shahjalal-mahmud <mahmud.nubtk@gmail.com>
2026-05-26 00:37:38 +0600

    Reintroduce FixCompileError.kt with a main function to print a chat message

---
+++ Unit_1/Introduction to Kotlin/StringTemplates.kt
@@ -0,0 +1,9 @@
+fun main() {
+    var discountPercentage: Double = 0.0
+    var offer: String = ""
+    var item = "Google Chromecast"
+    discountPercentage = 20.0
+    offer = "Sale - Up to $discountPercentage% discount on $item! Hurry up!"
+    
+    println(offer)
+}
