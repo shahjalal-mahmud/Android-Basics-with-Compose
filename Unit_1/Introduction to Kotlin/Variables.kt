44846e239d28e0aa89edf181db6e1f05fe9d0e3c
shahjalal-mahmud <mahmud.nubtk@gmail.com>
2026-05-26 00:21:19 +0600

    Add Variables.kt with a main function demonstrating variable usage and conditional statements

---
+++ Unit_1/Introduction to Kotlin/Variables.kt
@@ -0,0 +1,13 @@
+fun main() {
+    val numberOfPhotos = 10
+    val photosDeleted = 5
+    println("$numberOfPhotos Photos")
+    println("Deleted $photosDeleted")
+
+    val notificationsEnabled = false
+    if(notificationsEnabled){
+        println("Notification Enabled")
+    }else{
+        println("Notification not Enabled")
+    }
+}
