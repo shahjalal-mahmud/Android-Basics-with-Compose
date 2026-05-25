fa16e9b33e8cc67a619ea42598e18c24f027a095
shahjalal-mahmud <mahmud.nubtk@gmail.com>
2026-05-26 00:55:25 +0600

    Add DefaultParameters.kt with main function and displayAlertMessage for user sign-in alerts

---
+++ Unit_1/Introduction to Kotlin/DefaultParameters.kt
@@ -0,0 +1,28 @@
+fun main() {
+    val firstUserEmailId = "user_one@gmail.com"
+
+    // The following line of code assumes that you named your parameter as emailId. 
+    // If you named it differently, feel free to update the name.
+    println(displayAlertMessage(emailId = firstUserEmailId))
+    println()
+
+    val secondUserOperatingSystem = "Windows"
+    val secondUserEmailId = "user_two@gmail.com"
+
+    println(displayAlertMessage(secondUserOperatingSystem, secondUserEmailId))
+    println()
+
+    val thirdUserOperatingSystem = "Mac OS"
+    val thirdUserEmailId = "user_three@gmail.com"
+
+    println(displayAlertMessage(thirdUserOperatingSystem, thirdUserEmailId))
+    println()
+}
+
+fun displayAlertMessage(operatingSystem: String, emailId: String): String {
+    return "There's a new sign-in request on $operatingSystem for your Google Account $emailId"
+}
+
+fun displayAlertMessage(emailId: String): String {
+    return "There's a new sign-in request on Unknown OS for your Google Account $emailId"
+}
